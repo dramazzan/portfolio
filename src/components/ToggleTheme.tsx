48209618dca520a89829eaa9b91d755444f5278f
ramazandautbek <ramazandautbek04@gmail.com>
2025-05-22 22:47:58 +0500

    refactor(toggle_theme): updated toggle theme component

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+'use client'
+
+import { useTheme } from "next-themes"
+import { useEffect, useState } from "react"
+import { Moon, Sun } from "lucide-react"
+import styles from '@/styles/ToggleTheme.module.css'
 
 const ToggleTheme = () => {
-  return (
-    <div>
-        <button>Theme</button>
-    </div>
-  )
+    const { theme, setTheme } = useTheme()
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) return null
+
+    return (
+        <div>
+            <button 
+                className={styles.themeToggleButton}
+                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+                {theme === 'dark' ? <Moon size={20} /> : <Sun size={20} />}
+            </button>
+        </div>
+    )
 }
+
 
 export default ToggleTheme
