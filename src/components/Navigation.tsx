4f2e21929064be83a6ee4b79066ace17923b9912
ramazandautbek <ramazandautbek04@gmail.com>
2025-05-26 21:21:13 +0500

    refactor(navigation):change navigation style on tailwind

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,26 +1,33 @@
-import Link from "next/link"
-import ToggleTheme from "@/components/ToggleTheme"
-import styles from '@/styles/Navigation.module.css'
+import Link from "next/link";
+import ToggleTheme from "@/components/ToggleTheme";
 
 const Navigation = () => {
   return (
-    <div className={`bg-background p-2 ${styles.navigationContainer}`}>
-      <nav className={`bg-background ${styles.navigationBar}`}>
-        <h1 className={styles.navigationTitle}>Portfolio</h1>
+      <div className="w-screen p-2 flex items-center justify-center shadow transition-colors duration-200 ease-in-out">
+        <nav className="w-full max-w-[1200px] flex items-center justify-between">
+          <h1 className="text-2xl font-bold">Portfolio</h1>
+          <ul className="flex items-center gap-6 list-none">
+            <li>
+              <Link
+                  href="/"
+                  className="transition-colors duration-200 text-black dark:text-white hover:text-blue-400"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                  href="#about"
+                  className="transition-colors duration-200 text-black dark:text-white hover:text-blue-400"
+              >
+                About
+              </Link>
+            </li>
+          </ul>
+          <ToggleTheme />
+        </nav>
+      </div>
+  );
+};
 
-        <ul className={styles.navigationMenu}>
-          <li>
-            <Link href="/" className={styles.navigationLink}>Home</Link>
-          </li>
-          <li>
-            <Link href="#about" className={styles.navigationLink}>About</Link>
-          </li>
-        </ul>
-
-        <ToggleTheme />
-      </nav>
-    </div>
-  )
-}
-
-export default Navigation
+export default Navigation;
